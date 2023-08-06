5a8d8d2451c15ae4ab2b1cdf140ed765eb69e80a
Hyunjung Kim <withcusco@gmail.com>
2023-08-06 11:40:54 +0900

    [docs]day1 완료

---
+++ Day1-사칙연산/몫 구하기.js
@@ -0,0 +1,9 @@
+function solution(num1, num2) {
+  var answer = Math.floor(num1 / num2);
+  return answer;
+}
+
+// 몫을 구하는 문제이므로 Math.floor (내림)
+// Math.ceil(올림)
+// Math.floor(내림)
+// Math.round(반올림)
