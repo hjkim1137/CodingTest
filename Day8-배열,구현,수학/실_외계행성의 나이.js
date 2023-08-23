18469d568a0cb6e8a380786f5e5e243838c6f8bd
Hyunjung Kim <withcusco@gmail.com>
2023-08-23 15:31:47 +0900

    [docs] day8-외계행성의나이 주석 수정

--- Day8-배열,구현,수학/실_외계행성의 나이.js
+++ Day8-배열,구현,수학/실_외계행성의 나이.js
@@ -30,7 +30,9 @@ function solution(age) {
 1. forEach는 배열의 각 요소에 대해 함수를 실행하기 위한 메서드이다. 그러나 그 자체로는 값을 반환하지 않는다. 즉, forEach의 반환 값은 undefined이다.
 작업의 결과로 어떤 값을 직접 반환하려는 경우에는(이 예제에서도 반드시 return값이 있어야 하므로) 이럴 때는 map, filter, reduce와 같은 다른 배열 메서드를 사용하는 것이 더 적합하다.
 
-2. join 메소드의 역할과 예제:
+2. join 메소드의 역할과 예제
+배열을 전체 하나의 문자열로 바꿀 때 사용한다.
+
 let fruits = ['apple', 'banana', 'cherry'];
 
 let result1 = fruits.join();      // "apple,banana,cherry"
