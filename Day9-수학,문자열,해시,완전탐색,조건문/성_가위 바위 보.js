28cdd7817330a731c9f6375ee00a3ac4f7e1eb4d
Hyunjung Kim <withcusco@gmail.com>
2023-08-29 22:50:45 +0900

    [docs] day9- 가위 바위 보 완료

---
+++ Day9-수학,문자열,해시,완전탐색,조건문/성_가위 바위 보.js
@@ -0,0 +1,11 @@
+function solution(rsp) {
+  let arr = Array.from(rsp);
+
+  let win = (el) => {
+    if (el == '2') return '0';
+    else if (el == '0') return '5';
+    else if (el == '5') return '2';
+  };
+  let newArr = arr.map((el) => win(el)); //["0","5","2"]
+  return newArr.join('');
+}
