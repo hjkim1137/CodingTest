7bd26bc48dedbbf5bdbb06552af8d0714022f9c5
Hyunjung Kim <withcusco@gmail.com>
2023-09-07 09:25:06 +0900

    [docs] 공 던지기 문제 풀이 중

--- Day10-조건문,배열,수학,시뮬레이션/공 던지기.js
+++ Day10-조건문,배열,수학,시뮬레이션/공 던지기.js
@@ -4,10 +4,16 @@ function solution(numbers, k) {
 
   for (let i = 0; i < numbers.length; i += 2) {
     answer.push(numbers[i]); // [1,2,3,4,5,6] --> [1,3,5]
-    if (answer.length < k) {
-      let mylength = k - answer.length; // 5-3 =2
-      result = answer[mylength]; // answer[1] =3
-    } else result = answer[k - 1];
+    // answer의 길이가 짝수 일때(배열 내 쓰는 수만 쓴다)
+    // k의 길이가 answer 보다 짧거나 같은 경우
+    if (k <= answer.length) {
+      result = answer[k - 1];
+    }
+    // k의 길이가 answer 보다 긴 경우
+    else if (k > answer.length) {
+      result = answer[k - answer.length - 1];
+    }
+    // answer의 길이가 홀수 일때(배열 내 모든 수를 쓴다)
   }
   return result;
 }
